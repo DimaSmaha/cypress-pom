3e4f3b14bd9e4954a731048a5b28b3ad4694d11b
DimaSmaha <dmytrosmaha@gmail.com>
2025-03-19 19:54:06 +0200

    add conf

---
+++ smoke.config.ts
@@ -0,0 +1,28 @@
+require("dotenv").config();
+import { defineConfig } from "cypress";
+
+const env = {
+  BASE_URL: process.env.BASE_URL,
+  VALID_USER_LOGIN: process.env.VALID_USER_LOGIN,
+  VALID_USER_PASSWORD: process.env.VALID_USER_PASSWORD,
+};
+
+export default defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  defaultCommandTimeout: 10000,
+  e2e: {
+    baseUrl: `${Cypress.env("BASE_URL")}`,
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "cypress/e2e/smoke-tests/*.spec.ts",
+    video: true,
+  },
+  retries: {
+    runMode: 3,
+    openMode: 0,
+  },
+  env,
+});
